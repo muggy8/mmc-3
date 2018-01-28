6f95f622436bf3623988eb3b6eb583e8aad3594d
muggy8 <muggy8@gmail.com>
2018-01-28 01:16:47 -0800

    success making parts of the functions needed for data storage

--- dev/actions/data-store.php
+++ dev/actions/data-store.php
@@ -2,20 +2,64 @@
 	include_once("./configs.php"); // defines db settings
 	include_once("./polyfills.php"); // defines db settings
 
-	$conn = new mysqli($db_server, $db_user, $db_pass, $db_name);
+	class storage {
+		protected static $conn = null;
 
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
+		protected function __construct(){}
+
+	    protected function __clone(){}
+
+		public static function connect(&$db_server, &$db_user, &$db_pass, &$db_name){
+			if (self::$conn){
+				return;
+			}
+			self::$conn = new mysqli($db_server, $db_user, $db_pass, $db_name);
+			if (self::$conn->connect_error) {
+				die("Connection failed: " . $conn->connect_error);
+			}
+		}
+
+		public static function generateId($length = 32){
+			$allowedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_";
+	    	$id = $allowedCharacters[random_int(0, 51)];
+	    	for($i = 1; $i < $length; $i++){
+	        	$id = $id . $allowedCharacters[random_int(0, 62)];
+	    	}
+	    	return $id;
+		}
+
+		protected static function getObjectDefinition($type){
+			$statement = self::$conn->prepare("Select * from `mmc_3_types` where `mmc_3_types`.`type` = ?");
+			$statement->bind_param("s", $type);
+			if (!$statement->execute()){
+				die("failed to get object definition");
+			}
+			$res = $statement->get_result();
+			$resRows = mysqli_fetch_all($res);
+			$resRowsCount = count($resRows);
+			mysqli_free_result($res);
+			if (!$resRowsCount){
+				return null;
+			}
+			if ($resRowsCount === 1){
+				return $resRows[0];
+			}
+			return null;
+		}
+
+		public static function createObjectDefinition($type){
+			self::$conn
+		}
+
+		public static function storeObject($type, $obj){
+			self::$conn;
+			$objectDefinition = self::getObjectDefinition($type);
+			if (!$objectDefinition){
+				self::createObjectDefinition($type);
+				$objectDefinition = self::getObjectDefinition($type);
+			}
+		}
 	}
 
-	function generateId($length = 32){
-		$allowedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_";
-    	$id = $allowedCharacters[random_int(0, 51)];
-    	for($i = 1; $i < $length; $i++){
-        	$id = $id . $allowedCharacters[random_int(0, 62)];
-    	}
-    	return $id;
-	}
-
-	//print_r($allowedCharacters[random_int(0, 51)]);
-	print_r(generateId());
+	storage::connect(db_server, db_user, db_pass, db_name);
+	print_r(storage::createObjectDefinition("user"));
