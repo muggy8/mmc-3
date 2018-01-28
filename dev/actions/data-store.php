841a227c5e823f37a027312c942c6728f41d691f
muggy8 <muggy8@gmail.com>
2018-01-27 23:12:02 -0800

    try to get an ID generator working

--- dev/actions/data-store.php
+++ dev/actions/data-store.php
@@ -1,2 +1,21 @@
 <?php
-	include_once("./configs.php");
+	include_once("./configs.php"); // defines db settings
+	include_once("./polyfills.php"); // defines db settings
+
+	$conn = new mysqli($db_server, $db_user, $db_pass, $db_name);
+
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	}
+
+	$allowedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_";
+
+	function generateId($length = 16){
+    	$id = allowedCharacters[random_int(0, 51)];
+    	for($i = 1; $i < $length; $i++){
+        	$id .= allowedCharacters[random_int(0, 62)];
+    	}
+    	return $id;
+	}
+
+	echo generateId();
