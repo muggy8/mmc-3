454ea9af9dc56be1f76eb8b207a331554fd2d478
muggy8 <muggy8@gmail.com>
2018-02-05 00:28:16 -0800

    pull config from different place

--- dev/actions/data-store.php
+++ dev/actions/data-store.php
@@ -1,5 +1,5 @@
 <?php
-	include_once("./configs.php"); // defines db settings
+	include_once("../../configs.php"); // defines db settings
 	include_once("./polyfills.php"); // defines db settings
 
 	class storage {
