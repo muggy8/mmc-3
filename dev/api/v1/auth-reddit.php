ab15a4ec63c3f34230170ec552013ade432482ad
muggy8 <muggy8@gmail.com>
2018-03-07 23:14:17 -0800

    typo

--- dev/api/v1/auth-reddit.php
+++ dev/api/v1/auth-reddit.php
@@ -1,6 +1,6 @@
 <?php
 	include_once("globals.php");
-	include_once(app_root . "/api/v1/init.php");
+	include_once(api_root . "/api/v1/init.php");
 
 	if (!$_GET["code"]){
 		header("location: https://www.reddit.com/api/v1/authorize?client_id=" . reddit_app_id . "&response_type=code&state=" . storage::generateId() . "&redirect_uri=http://mugdev.com/mmc-3/dev/api/v1/auth-reddit.php&duration=temporary&scope=identity");
