5011d04663fc9dd05b2b5751e50fb2d79d2255fe
muggy8 <muggy8@gmail.com>
2018-03-15 20:00:11 -0700

    ping?

--- dev/api/v1/ping.php
+++ dev/api/v1/ping.php
@@ -1,5 +1,12 @@
 <?php
 	include_once("globals.php");
 	include_once(api_root . "/api/v1/init.php");
-
+	
+	echo "<pre>";
+	print_r($query);
+	
+	print_r($headers);
+	
 	print_r($body);
+
+	echo "</pre>";
