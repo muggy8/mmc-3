c0711a0ca6e0e96521058e133057e9eb59aa5f7c
muggy8 <muggy8@gmail.com>
2018-03-16 20:38:32 -0700

    events to handle the actual php actions

---
+++ dev/actions/event.php
@@ -0,0 +1,27 @@
+<?php 
+	function event($complex){
+		static $listeners = null;
+		static $includeQueue = [];
+		static $runListener = null;
+		static $workspace = null;
+		
+		if (!$listeners){
+			$path = $complex;
+			$workspace = (object)[];
+			$runListener = function($path, &$workspace){
+				include_once($path);
+			};
+			// get the listeners from the path
+		}
+		
+		$currentQueueLength = count($includeQueue);
+		foreach($listener as $name => $path){
+			// push the correct listeners onto the queue
+		}
+		
+		if ($currentQueueLength === 0){
+			while($included=array_shift($includeQueue)){
+				$runListener($included, $workspace);
+			}
+		}
+	}
