f9bdd24dd30027fd7d2563f8497412f3f31f74c4
muggy8 <muggy8@gmail.com>
2018-03-16 20:36:44 -0700

    renamed responce and completed its implementation

--- dev/actions/response.php
+++ dev/actions/response.php
@@ -1,35 +1,45 @@
 <?php
 	class response {
 		protected static $body = "";
-		protected static $headers = $null;
+		protected static $headers = null;
 		protected static $streaming = false;
 
 		protected static function initOnce(){
 			static $init = false;
-
 			if (!$init){
 				$init = true;
-				$headers = (object)[];
+				self::$headers = (object)[];
 			}
 		}
 
-		public static addHeader($name, $val){
+		public static function addHeader($name, $val){
 			self::initOnce();
 			// add the header or initialize it if it's not set
+			self::$headers->{$name} = self::$headers->{$name} ?: [];
+			array_push(self::$headers->{$name}, $val);
+
 		}
-		public static setHeader($name, $val){
+		public static function setHeader($name, $val){
 			self::initOnce();
 			// ovewrite the current header and set it's value
+			self::$headers->{$name} = [$val];
 		}
 
-		public static stream(){
+		public static function stream(){
 			self::initOnce();
 			if (!self::$streaming){ // stream will only activate once and will start streaming the output
 				$streaming = true;
 				// send the headers and when future write chunks comes in, immediately echo it
+
+				foreach(self::$headers as $key => $val){
+					$val = implode(", ", $val);
+					header("$key: $val");
+				}
+
+				echo self::$body;
 			}
 		}
-		public static write($chunk){
+		public static function write($chunk = ""){
 			self::initOnce();
 			if (self::$streaming){
 				echo $chunk;
@@ -39,7 +49,7 @@
 			}
 		}
 
-		public static end($chunk){
+		public static function send($chunk = ""){
 			self::initOnce();
 			if (self::$streaming){
 				echo $chunk;
@@ -52,3 +62,11 @@
 			}
 		}
 	}
+
+    // response::addHeader("foo", "bar");
+    // response::addHeader("foo", "baz");
+    // response::setHeader("foo", "writeOver");
+    // response::write("abc123");
+    // response::write("\n\n");
+    // response::write("456xyz");
+    // response::send();
