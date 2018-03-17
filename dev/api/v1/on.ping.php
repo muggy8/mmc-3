852e4dfb7443c8db3596552ce44f96e34352fba7
muggy8 <muggy8@gmail.com>
2018-03-17 01:09:10 -0700

    fixed one small bug that was causing urls that ended in "/" to not trigger the right things

--- dev/api/v1/on.ping.php
+++ dev/api/v1/on.ping.php
@@ -1,6 +1,6 @@
 <?php
 	response::write("<pre>") ;
 
-	response::write(json_encode(request()));
+	response::write(json_encode(request(), JSON_PRETTY_PRINT));
 
 	response::write("</pre>") ;
--- dev/api/v1/on.request.php
+++ dev/api/v1/on.request.php
@@ -5,6 +5,9 @@
 		$matchRes
 	);
 	request("endPoint", str_replace('api/' . api_version . '/', '', $matchRes[0]));
+	if(subStr(request("endPoint"), -1 ) === "/"){
+		request("endPoint", subStr(request("endPoint"), 0, -1 ));
+	}
 
 	event(request("method"));
 	event(request("method") . "." . str_replace('/', ".", request("endPoint")));
