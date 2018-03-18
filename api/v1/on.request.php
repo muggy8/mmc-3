4edb0bc8d35b18012b6d4a4eab8ce2d1875a092b
muggy8 <muggy8@gmail.com>
2018-03-18 14:06:24 -0700

    changed how the internal routing works for hte api events to support versioned or unversioned requests and query and search params

--- api/v1/on.request.php
+++ api/v1/on.request.php
@@ -1,15 +1,15 @@
 <?php
 	preg_match(
-		'/api\/' . api_version . '\/(.+)?$/',
+		'/api\/(v\d+\/)?([^\?\#]+)?/',
 		request("url"),
 		$matchRes
 	);
-	request("endPoint", str_replace('api/' . api_version . '/', '', $matchRes[0]));
+	request("endPoint", $matchRes[2]);
 	if(subStr(request("endPoint"), -1 ) === "/"){
 		request("endPoint", subStr(request("endPoint"), 0, -1 ));
 	}
 
-	event(request("method") . ".api");	
+	event(request("method") . ".api");
 	// var_dump(request("method") . ".api");
 	event(request("method") . ".api." . str_replace('/', ".", request("endPoint")));
 	// var_dump(request("method") . ".api." . str_replace('/', ".", request("endPoint")));
