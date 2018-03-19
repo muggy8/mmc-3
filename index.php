7415c6d338c0a72c1b764d6751aabb269eab3285
muggy8 <muggy8@gmail.com>
2018-03-18 22:29:12 -0700

    renamed index.html to index.php and output the console script conditionally

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <html>
 	<head>
 		<title>Music Matrix Composer V3</title>
+
+		<?=$_GET["console"] ? '<script src="//unpkg.com/mobile-inspector">{}</script>' : null ?>
 		<script src="//unpkg.com/aja@0.4.1/aja.min.js"></script>
 	</head>
 	<body></body>
