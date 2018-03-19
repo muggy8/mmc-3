780228ee4ad82fca690a4a36ea3276c15dfe9737
Muggy Ate <muggy8@gmail.com>
2018-03-19 08:42:48 -0700

    responsive meta headers

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 	<head>
 		<title>Music Matrix Composer V3</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<?=$_GET["console"] ? '<script src="//unpkg.com/mobile-inspector">{}</script>' : null ?>
 		<script src="//unpkg.com/aja@0.4.1/aja.min.js"></script>
 		<script src="//unpkg.com/onfontready"></script>
