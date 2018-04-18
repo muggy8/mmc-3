0dadde282f0474e1ab29e0723ad2f08f59ebac31
muggy8 <muggy8@gmail.com>
2018-04-17 23:57:53 -0700

    loging out now works properly

--- api/v1/auth/on.api.auth.logout.php
+++ api/v1/auth/on.api.auth.logout.php
@@ -1,13 +1,13 @@
 <?php
-	if (request("user") && request("cookies")){
-		request("user")->sessions = (object)request("user")->sessions; // make this an object from an array if it's not already for whatever reason and keep the reference to each other
-		unset(request("user")->sessions->{request("cookies")->session});
-		storage::storeObject(request("userId"), request("user"));
+	if (request("user") && request("sessionId")){
+		storage::deleteObject(request("sessionId"));
 		setcookie("user", null, 0, "/", $_SERVER["HTTP_HOST"]);
 		setcookie("session", null, 0, "/", $_SERVER["HTTP_HOST"]);
 		response::setHeader("Status", "204 No Content");
+
 	}
 	else {
+		response::setHeader("Status", "403 Forbidden");
 		$workspace->errors = $workspace->errors ?: [];
 		array_push($workspace->errors, "Not currently authenticated");
 	}
