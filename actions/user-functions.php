278d38f2608406e2ab1cacc7db08489a6e8ac4e4
muggy8 <muggy8@gmail.com>
2018-04-18 00:02:15 -0700

    more empty user functions

--- actions/user-functions.php
+++ actions/user-functions.php
@@ -1,4 +1,17 @@
 <?php
 	function public_user($userObj){
+		unset($userObj->identity);
 		return $userObj;
+	}
+
+	function strip_user($user){
+
+	}
+
+	function validate_user($user){
+
+	}
+
+	function patch_user($original, $patch){
+
 	}
