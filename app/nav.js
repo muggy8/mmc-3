9534f40c5f8a460b05ba6b51dbac21cb0c878c9e
muggy8 <muggy8@gmail.com>
2018-04-18 09:37:44 -0700

    check the cookie of user and fetch user id if it exists

--- app/nav.js
+++ app/nav.js
@@ -1,5 +1,14 @@
 var navView = (function(){
     var view = proxymity(document.querySelector("#header-nav"))
-    
+    var cookies = keyValParse(document.cookie)
+
+    if (cookies.user){
+        aja()
+            .url("/api/user/me")
+            .on("2xx", function(res){
+                console.log(res)
+            })
+    }
+
     return view
 })()
