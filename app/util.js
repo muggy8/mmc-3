2d64c90fd4e2bf0bd6ed16bf60715299a99166aa
muggy8 <muggy8@gmail.com>
2018-04-18 09:29:50 -0700

    keyval method in  utils

---
+++ app/util.js
@@ -0,0 +1,9 @@
+function keyValParse(str, seperator = "&"){
+	var output = {}
+	var pairs = str.split(new RegExp("\\s*" + seperator + "\\s*"))
+	for(var pair of pairs){
+		pair = pair.split("=")
+		output[pair[0]] = pair[1]
+	}
+    return output
+}
