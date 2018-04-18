ae9bbba884947712d6d9074e65f8ce36cb35180e
muggy8 <muggy8@gmail.com>
2018-04-17 23:26:32 -0700

    redirect manually derr

--- api/v1/auth/on.api.auth.reddit.php
+++ api/v1/auth/on.api.auth.reddit.php
@@ -1,10 +1,12 @@
 <?php
 	// check to see if the user is already logged in and doesn't have a reddit authentication then ya we dont need to relog so we dont send them to get reddit to authorize us
 	if (request("user")->auth->reddit){
+		response::setHeader("Status", "302 Found");
 		response::addHeader("location", "/");
 	}
 
 	// if the user is not logged in or doesn't have a reddit authentication, we should ask reddit to identify this guy for us
 	else {
+		response::setHeader("Status", "302 Found");
 		response::addHeader("location", "https://www.reddit.com/api/v1/authorize?client_id=" . reddit_app_id . "&response_type=code&state=" . storage::generateId() . "&redirect_uri=https://mmc-3.mugdev.com/api/auth/reddit/callback&duration=temporary&scope=identity");
 	}
