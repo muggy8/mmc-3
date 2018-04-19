eddc5a47ff4b3a0fc8b9d9f075ce98b4fb785167
muggy8 <muggy8@gmail.com>
2018-04-18 23:44:02 -0700

    user api begins with initializing all the global user params

--- api/v1/user/on._.api.user__.php
+++ api/v1/user/on._.api.user__.php
@@ -1,2 +1,12 @@
 <?php
-	include_once(api_root . "/actions/user-functions.php");
+	$workspace->publicUserSchema = (object)[
+		"name" => true,
+		"email" => true,
+		"songs" => true
+	];
+	
+	$workspace->updateUserSchema = (object)[
+		"name" => "string",
+		"email" => "string",
+		"songs" => "string"
+	];
