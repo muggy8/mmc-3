b1be355e712c7b7c4b83088c490fbdaff92ea4aa
Muggy Ate <muggy8@gmail.com>
2018-04-18 19:09:21 -0700

    trying to figure out why the endpoint wont work

--- api/v1/user/on.get.api.user._.php
+++ api/v1/user/on.get.api.user._.php
@@ -1,8 +1,9 @@
 <?php
 	response::setHeader("Status", "200 OK");
-	preg_match('/^\/user\/([^\/]*)/', request("endpoint"), $userMatch);
+	preg_match('/^\/user\/([^\/]*)$/', request("endpoint"), $userMatch);
 	$potentialId = "user:" . $userMatch[1];
 	if ($user = storage::get($potentialId)){
 		response::setHeader("Status", "200 OK");
+		response::setHeader("x-user-id", $potentialId);
 		response::write(json_encode(public_user($user)));
 	}
