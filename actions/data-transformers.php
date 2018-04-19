e0586e60b0a67db60dc20c33deb21825dead22d1
muggy8 <muggy8@gmail.com>
2018-04-18 22:29:20 -0700

    some test code and copied the schema stripper to schema checker

--- actions/data-transformers.php
+++ actions/data-transformers.php
@@ -18,4 +18,48 @@
 		return $data;
 	}
 
+	function patch($original, $incoming){
+
+	}
+
+	function withinSchema($data, $schema){
+		$assumption = true;
+		foreach($data as $key => $val){
+			if (isset($schema->{$key})){
+				if (is_object($val) && is_object($schema->{$key})){
+					$assumption = $assumption && withinSchema($val, $schema->{$key});
+				}
+				else if (is_array($val) && is_object($schema->{$key})){
+					foreach($val as &$item){
+						$assumption = $assumption && withinSchema($item, $schema->{$key});
+					}
+				}
+			}
+			else {
+				$assumption = false;
+			}
+		}
+		return $assumption;
+	}
+
+
 	$output = withinSchema((object)[
+		"name" => "abc123",
+		"arr" => [
+			(object)[
+				"foo" => 123,
+				"bar" => 456
+			],
+			(object)[
+				"foo" => 888,
+				"bar" => 999
+			]
+		],
+		"junk" => "1234567890"
+	], (object)[
+		"name" => true,
+		"arr" => (object)[
+			"foo" => true
+		]
+	]);
+	print_r($output);
