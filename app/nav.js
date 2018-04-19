9e902d087f6fde55cc2d30c2f876cce5b58e9842
muggy8 <muggy8@gmail.com>
2018-04-18 17:21:00 -0700

    login logout is now doable

--- app/nav.js
+++ app/nav.js
@@ -1,3 +1,8 @@
 ;(function(accountableData){
-
+    accountableData.logout = function(){
+        aja()
+            .url("/api/auth/logout")
+            .on("2xx", utils.updateUser)
+            .go()
+    }
 })(mmc.nav)
