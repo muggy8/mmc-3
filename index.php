e9acfa3e679ea2548dd8a16c3f150132efb99c4a
Muggy Ate <muggy8@gmail.com>
2018-04-19 08:19:30 -0700

    deleted accidentally created files

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-	header("Content-Type: application/javascript;charset=UTF-8");
-	include_once "main.js";
-	include_once "nav.js";
--- nav.js
+++
@@ -1,3 +0,0 @@
-var navView = (function(){
-    var view = proxymity(document.querySelector("#header-nav"))
-})()
