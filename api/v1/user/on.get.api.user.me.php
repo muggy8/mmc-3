dfe1c875631c968fcc29200a3d199d0d60058445
muggy8 <muggy8@gmail.com>
2018-04-20 15:52:36 -0700

    prepping to move the /user/me to just me for performance efficiency

--- api/v1/user/on.get.api.user.me.php
+++ api/v1/user/on.get.api.user.me.php
@@ -1,6 +1,11 @@
 <?php
+    include_once "on._.api.user__.php"; // it's not like we cant just include this normally anyways
 	if (request("user")){
 		response::setHeader("Status", "200 OK");
 		response::write(json_encode(stripTo(request("user"), $workspace->publicUserSchema)));
 	}
-	// the 403 is handled by the get user api
+	else {
+		response::setHeader("Status", "403 Forbidden");
+		$workspace->errors = $workspace->errors ?: [];
+		array_push($workspace->errors, "Not currently authenticated");
+	}
