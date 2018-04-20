0a9c7c0517f6e555d4484c9292659f74e395e1c1
muggy8 <muggy8@gmail.com>
2018-04-20 15:54:32 -0700

    ok moved things around on the end points

---
+++ api/v1/user/on._.api.me.php
@@ -0,0 +1,2 @@
+<?php
+    include_once "on._.api.user__.php"; // it's not like we cant just include this normally anyways
--- api/v1/user/on.get.api.me.php
+++ api/v1/user/on.get.api.me.php
@@ -1,5 +1,4 @@
 <?php
-    include_once "on._.api.user__.php"; // it's not like we cant just include this normally anyways
 	if (request("user")){
 		response::setHeader("Status", "200 OK");
 		response::write(json_encode(stripTo(request("user"), $workspace->publicUserSchema)));
