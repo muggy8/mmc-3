683601c91f789ee183fef20cd6f3ab0f247a6925
muggy8 <muggy8@gmail.com>
2018-04-20 18:28:37 -0700

    only hard code things once when building the batch output

--- app/js/index.php
+++ app/js/index.php
@@ -1,5 +1,9 @@
 <?php
 	header("Content-Type: application/javascript;charset=UTF-8");
-	include_once "util.js";
-	include_once "main.js";
-	include_once "nav.js";
+    include_once "globals.php";
+    $assets = json_decode(file_get_contents(http_root . "/app/assets.json"));
+    foreach($assets->js as $js){
+        if (is_file(http_root . $js)){
+            include_once http_root . $js;
+        }
+    }
