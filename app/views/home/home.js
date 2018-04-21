70efc5f71bb24bdd45aca3c6bc8ea1b82486ba55
muggy8 <muggy8@gmail.com>
2018-04-20 19:35:59 -0700

    load thing into a temporary loading thing so it doesn't show up

--- app/views/home/home.js
+++ app/views/home/home.js
@@ -1,9 +1,9 @@
 void function(homeScope){
     aja()
         .url("/app/views/home/home.html")
-        .into("main")
+        .into("#loading")
         .on("2xx", function(){
-            homeScope.view = proxymity(main.childNodes, momoca)
+            homeScope.view = proxymity(loading.childNodes, momoca).detach()
         })
         .on("4xx", function(o3o){
             momoca.notify("failed to get home view")
