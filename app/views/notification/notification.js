61685a1e7a297509927b3317d73afeb2502ee203
muggy8 <muggy8@gmail.com>
2018-04-20 20:24:38 -0700

    working on adding pop up notifications to the app

---
+++ app/views/notification/notification.js
@@ -0,0 +1,36 @@
+void function(){
+    var notificationsView
+    var notificationController = {
+        notifications: []
+    }
+
+    function notificationsArray(){
+        return notificationsView
+            ? notificationsView.app.notifications
+            : notificationController.notifications
+    }
+
+    momoca.notify = function(message, configs = {}){
+        notificationsArray().push(message)
+        if (typeof configs.timeout === "number"){
+            setTimeout(function(){
+                var source = notificationsArray()
+                source.splice(source.indexOf(message), 1)
+            }, timeout)
+        }
+    }
+
+    aja()
+        .url("/app/views/notification/notification.html")
+        .into("#loading")
+        .on("2xx", function(){
+            notificationsView = proxymity(loading.childNodes, notificationController).appendTo("body")
+        })
+        .on("4xx", function(){
+            alert("Failed to load Notification module")
+        })
+        .on("5xx", function(){
+            alert("Server Error")
+        })
+
+}()
