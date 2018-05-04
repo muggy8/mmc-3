302b4ba52952c5356f77d2e2a33ad1614679144a
muggy8 <muggy8@gmail.com>
2018-05-03 17:32:39 -0700

    more or less finished the super basic pop-over

--- app/views/pop-over/pop-over.js
+++ app/views/pop-over/pop-over.js
@@ -1,9 +1,16 @@
 aja()
 	.url("/app/views/pop-over/pop-over.html")
 	.into("#loading")
+	.on("5xx", function(){
+		momoca.notify("Server Error")
+	})
+	.on("4xx", function(){
+		momoca.notify("Failed to load pop-over module")
+	})
 	.on("2xx", function(){
 		var popoverView = proxymity(momoca.loading.childNodes)
 		var popOverController = popoverView.app
+        popOverController.body = popOverController.innerHTML = undefined;
 		momoca.popOver = function(body, configs = {}){
 			if (body instanceof HTMLElement){
 				popOverController.body = body
@@ -11,13 +18,13 @@ aja()
 			else if (typeof body === "string") {
 				popOverController.innerHTML = body
 			}
-			popOverController
+            popOverController.configs = configs
+			popoverView.appendTo("main")
 		}
-	})
-	.on("4xx", function(){
-		momoca.notify("Failed to load pop-over module")
-	})
-	.on("5xx", function(){
-		momoca.notify("Server Error")
+
+        popOverController.close = function(){
+            popoverView.detach()
+            popOverController.body = popOverController.innerHTML = undefined;
+        }
 	})
 	.go()
