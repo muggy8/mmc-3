93ebaa7a0f8ed439632e87e87e0990c3ccb5299d
muggy8 <muggy8@gmail.com>
2018-06-22 22:46:52 -0700

    ok got rid of more account related stuff and added our own xhr system

--- app/js/util.js
+++ app/js/util.js
@@ -1,13 +1,4 @@
 var utils = {
-	keyValParse: function(str, seperator = "&"){
-		var output = {}
-		var pairs = str.split(new RegExp("\\s*" + seperator + "\\s*"))
-		for(var pair of pairs){
-			pair = pair.split("=")
-			output[pair[0]] = pair[1]
-		}
-		return output
-	},
 	extendFn: function(superFunction, extendedFunction){
 		return function(){
 			var args = Array.prototype.slice.call(arguments)
@@ -35,5 +26,6 @@ var utils = {
 		}
 		range.push(start)
 		return range
-	}
+	},
+	xhr: chainify(XMLHttpRequest)
 }
