bb501466deb35fe6a73b27ceddcda7cb445e0ccd
muggy8 <muggy8@gmail.com>
2018-06-22 22:45:45 -0700

    removed get user related stuff since we're going serverless

--- app/js/main.js
+++ app/js/main.js
@@ -1,25 +1,4 @@
 var mmcView = proxymity(document.querySelector("body"), {
-	getUser: function(){
-		var cookies = utils.keyValParse(document.cookie)
-		if (cookies.user){
-			aja()
-				.url("/api/me")
-				.cache(false)
-				.on("2xx", function(ouo){
-					momoca.user = ouo
-				})
-				.on("4xx", function(o3o){
-					momoca.user = {}
-				})
-				.on("5xx", function(x_x){
-					momoca.notify("Server Error. Please try again later")
-				})
-				.go()
-		}
-		else {
-			momoca.user = {}
-		}
-	},
 	notify: function(message){
 		console.log(message)
 	},
