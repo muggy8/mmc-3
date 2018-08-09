62987652beb3066ae959d4d69a675a9812537794
muggy8 <muggy8@gmail.com>
2018-08-09 14:24:04 -0700

    oops forgot to delete these guys

--- app/js/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-	header("Content-Type: application/javascript;charset=UTF-8");
-    include_once "globals.php";
-    $assets = json_decode(file_get_contents(http_root . "/app/assets.json"));
-    foreach($assets->js as $js){
-        if (is_file(http_root . $js)){
-            include_once http_root . $js;
-        }
-    }
